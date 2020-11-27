95ff5db6b67144881b21d4da6771c400a2e0758e
thorski1 <samuelthoyre@C02L94W0F6T6L.local>
2020-11-26 20:46:53 -0500

    fix issues with solar system css

--- src/components/SolarSystemLoading.tsx
+++ src/components/SolarSystemLoading.tsx
@@ -5,11 +5,11 @@ export const SolarSystemLoading = () => {
 		<div className="spinner-box">
 			<div className="solar-system">
 				<div className="earth-orbit orbit">
-					<div className="planet earth"></div>
+					<div className="planet"></div>
 					<div className="venus-orbit orbit">
-						<div className="planet venus"></div>
+						<div className="planet"></div>
 						<div className="mercury-orbit orbit">
-							<div className="planet mercury"></div>
+							<div className="planet"></div>
 							<div className="sun"></div>
 						</div>
 					</div>
